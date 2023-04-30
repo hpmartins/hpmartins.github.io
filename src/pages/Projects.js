8f36de593a3dc3a98361031c3d1bfe2a3cd6d98a
Henrique <hpmartins@gmail.com>
2023-04-30 22:36:39 +0000

    Added todo list.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,20 +5,26 @@ export default function Projects() {
     return (
         <div style={{maxWidth : 'fit-content'}}>
             <h4>Projects and features:</h4>
-
-            <ul>
-                <li>pyXRO: X-Ray Optics - <Link to="">[go to]</Link> <Link to="https://github.com/hpmartins/pyxro">[repository]</Link></li>
+            <ul style={{marginBottom:'12px'}}>
+                <li>pyXRO: X-Ray Optics - <Link to="">[open]</Link> <a href="https://github.com/hpmartins/pyxro" target="_blank" rel="noopener noreferrer">[src]</a></li>
                 <ul>
                     <li>Calculates photoelectron emission intensities for layered samples in the presence of x-ray optical effects</li>
                     <li>Multilayer sample building and visualization</li>
                     <li>Reflectivity, electric field, and photoemission intensities as a function of incident angle and/or photon energy</li>
                     <li>Based on YXRO - Yang <i>et al.</i> <a href="https://doi.org/10.1063/1.4790171">[doi]</a></li>
                 </ul>
-                <li style={{marginTop:'12px'}}>pyDTD: Dynamical Theory of Diffraction - <Link to="">[go to]</Link> <Link to="https://github.com/hpmartins/pydtd">[repository]</Link></li>
+                <li style={{marginTop:'12px'}}>pyDTD: Dynamical Theory of Diffraction - <Link to="">[open]</Link> <a href="https://github.com/hpmartins/pydtd" target="_blank" rel="noopener noreferrer">[src]</a></li>
                 <ul>
                     <li>Same as pyXRO, but for single-crystals,</li>
                     <li>Easy input: CIF files</li>
                 </ul>
+            </ul>
+
+            <h4>Soon (kind of todo list):</h4>
+            <ul>
+                <li>Convert their Dash interface to React</li>
+                <li>Merge their apps into a separate main app</li>
+                <li>Optimizer!</li>
             </ul>
         </div>
     )
